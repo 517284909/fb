0d7424117fdb743abf8450fb74503276c97e40f2
developeratdaguanyuan <developeratdaguanyuan@gmail.com>
2017-12-07 22:46:49 -0600

    Wild Card Matching

---
+++ dp/wildcardMatching.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int j = 0; j < p.length(); j++) {
+            if (p.charAt(j) != '*') break;
+            dp[0][j + 1] = true;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) != '*') {
+                    dp[i + 1][j + 1] = dp[i][j] && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?');
+                } else {
+                    dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1];
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
+
